97b580f400d5b8250d35f932c9fba3eeaa0ee896
vhoyer <vhoyer@live.com>
2017-07-13 15:07:07 -0300

    Sorted list with a space in the bottom
    Signed-off-by: vhoyer <vhoyer@live.com>

--- app/src/main/java/com/hpixel/dreamusicplayer/MainActivity.kt
+++ app/src/main/java/com/hpixel/dreamusicplayer/MainActivity.kt
@@ -9,12 +9,11 @@ import com.hpixel.dreamusicplayer.controller.SongsArrayAdaptor
 
 class MainActivity : AppCompatActivity() {
 
-    val context = this.applicationContext
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
+        val context = this.applicationContext
         val songs = songList()
         val songListView = findViewById(R.id.main_songList) as ListView
         val songListArrayAdapter = SongsArrayAdaptor(context, songs)
@@ -24,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     fun songList(): ArrayList<Song> {
         val files = Files()
 
+        val context = this.applicationContext
         val separator = "||"
         val songQuery = Song.query
 
@@ -35,6 +35,13 @@ class MainActivity : AppCompatActivity() {
             val newSong = Song(song, separator)
             songList.add(newSong)
         }
+
+        //sort "alphabetically" by unicode value
+        songList.sortBy { song -> song.title }
+
+        //due to a possible "floating action button" that may be added later
+        val anEmptySpaceInTheEnd = Song(0,"","","","","",0)
+        songList.add(anEmptySpaceInTheEnd)
 
         return songList
     }
