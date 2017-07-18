d341091a82ec44c71e959ce444d16a4e197fb6dc
vhoyer <vhoyer@live.com>
2017-07-18 16:26:51 -0300

    Added Activity Lifecycle Methods
    Signed-off-by: vhoyer <vhoyer@live.com>

--- app/src/main/java/com/hpixel/dreamusicplayer/view/PlayerActivity.kt
+++ app/src/main/java/com/hpixel/dreamusicplayer/view/PlayerActivity.kt
@@ -14,9 +14,14 @@ import com.hpixel.dreamusicplayer.controller.mediaplayer.MediaPlayerService
 import com.hpixel.dreamusicplayer.model.Current
 
 
+
+
+
+
 class PlayerActivity : AppCompatActivity() {
 
-    var player : MediaPlayerService? = null
+    private var player : MediaPlayerService? = null
+    private var serviceBound: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,11 +46,20 @@ class PlayerActivity : AppCompatActivity() {
         txtArtist.text = artist
         txtAlbum.text = album
         imgCover.setImageDrawable(artwork)
+        
+        playAudio(songToPlay.filePath)
+    }
 
-        val playerService = Intent(context, MediaPlayerService::class.java)
-
-        startService(playerService)
-        bindService(playerService, serviceConnection, Context.BIND_ABOVE_CLIENT)
+    private fun playAudio(media: String) {
+        //Check is service is active
+        if (!serviceBound) {
+            val playerIntent = Intent(this, MediaPlayerService::class.java)
+            startService(playerIntent)
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+        } else {
+            //Service is active
+            //Send media with BroadcastReceiver
+        }
     }
 
     //Binding this Client to the AudioPlayer Service
@@ -55,11 +69,30 @@ class PlayerActivity : AppCompatActivity() {
             // We've bound to LocalService, cast the IBinder and get LocalService instance
             val binder = service as MediaPlayerService.LocalBinder
             player = binder.service
-            //serviceBound = true
+            serviceBound = true
         }
 
         override fun onServiceDisconnected(name: ComponentName) {
-            //serviceBound = false
+            serviceBound = false
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putBoolean("ServiceState", serviceBound)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        serviceBound = savedInstanceState.getBoolean("ServiceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (serviceBound) {
+            unbindService(serviceConnection)
+            //service is active
+            player!!.stopSelf()
         }
     }
 }
